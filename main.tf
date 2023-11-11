cca1a24e773a26f0d76803b10c311149b57f622c
Misan Ikomi <misan4vhf@yahoo.com>
2023-11-11 01:07:39 +0100

    adding the ec2 code

--- main.tf
+++ main.tf
@@ -12,3 +12,12 @@ module "rds" {
   vpc_id = module.vpc.vpc_id
   vpc_cidr = "10.0.0.0/16"
 }
+
+module "ec2" {
+  source = "./ec2"
+  public_subnet_1 = module.vpc.public_subnet_1_id
+  vpc_id = module.vpc.vpc_id
+  vpc_cidr = "10.0.0.0/16"
+  tags = local.project-tags
+}
+
